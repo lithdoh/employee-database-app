30396586499aa3ae7ca646118b7ac65d71760ef7
lithdoh <lithifuse@gmail.com>
2023-03-29 19:00:05 -0700

    Change backend URI from heroku to

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -9,18 +9,20 @@ export class EmployeeService {
   constructor(private _http: HttpClient) {}
 
   addEmployee(data: any): Observable<any> {
-    return this._http.post('https://fake-server-app20.herokuapp.com/employees', data);
+    // Heroku endpoint
+    // https://fake-server-app20.herokuapp.com
+    return this._http.post('https://my-json-server.typicode.com/lithdoh/employees-database-json-server-typicode/employees', data);
   }
 
   updateEmployee(id: number, data: any): Observable<any> {
-    return this._http.put(`https://fake-server-app20.herokuapp.com/employees/${id}`, data);
+    return this._http.put(`https://my-json-server.typicode.com/lithdoh/employees-database-json-server-typicode/employees/${id}`, data);
   }
 
   getEmployeeList(): Observable<any> {
-    return this._http.get('https://fake-server-app20.herokuapp.com/employees');
+    return this._http.get('https://my-json-server.typicode.com/lithdoh/employees-database-json-server-typicode/employees');
   }
 
   deleteEmployee(id: number): Observable<any> {
-    return this._http.delete(`https://fake-server-app20.herokuapp.com/employees/${id}`);
+    return this._http.delete(`https://my-json-server.typicode.com/lithdoh/employees-database-json-server-typicode/employees/${id}`);
   }
 }
